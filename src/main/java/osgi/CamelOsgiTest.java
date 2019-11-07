dc995bee03c46938f50d2dbe14f662c3687813fd
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2019-11-07 22:25:02 +0100

    added new discoveries whilst playing around with Camel Main and CamelContext classes

--- src/main/java/osgi/CamelOsgiTest.java
+++ src/main/java/osgi/CamelOsgiTest.java
@@ -1,15 +1,35 @@
 package osgi;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.main.Main;
 
 public class CamelOsgiTest {
     public static void main(String... args) throws Exception {
-        Main main = new Main();
-        // TODO: find out how to support MDC logging
+        // Run a route using only Main.
+        // Not sure if MDC can be used here...
+//        Main main = new Main();
+//        main.addRouteBuilder(new CamelOsgiTestBlueprint());
+//        main.run();
+
+        // Run a route using only CamelContext.
+        // Note that the JVM will terminate before the route gets a chance to do its thing.
+        // This is because start() is non-blocking and if there are no statements following it,
+        // the end of the program is reached and the JVM terminates.
+        // This is why you see some code snippets using sleep methods.
 //        CamelContext context = new DefaultCamelContext();
 //        context.setUseMDCLogging(true);
-//        main.getCamelContexts().add(context);
-        main.addRouteBuilder(new CamelOsgiTestBlueprint());
+//        context.addRoutes(new CamelOsgiTestBlueprint());
+//        context.start();
+
+        // Best of both
+        CamelContext context = new DefaultCamelContext();
+        context.setUseMDCLogging(true);
+        context.addRoutes(new CamelOsgiTestBlueprint());
+        context.start();
+
+        Main main = new Main();
+        main.getCamelContexts().add(context);
         main.run();
     }
 }
