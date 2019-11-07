f9f408a7e27bda2bc55b81fb56851d44089c5b7c
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2019-11-07 21:32:06 +0100

    added period and started messing with log4j.properties

--- src/main/java/osgi/CamelOsgiTestBlueprint.java
+++ src/main/java/osgi/CamelOsgiTestBlueprint.java
@@ -6,7 +6,7 @@ import org.apache.camel.builder.RouteBuilder;
 public class CamelOsgiTestBlueprint extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-        from("timer:start")
+        from("timer:start?period=100")
                 .id("mainRoute")
                 .log(LoggingLevel.INFO, "com.asodc.camel.osgi.CamelOsgiTestBlueprint", "Headers: ${headers}");
     }
