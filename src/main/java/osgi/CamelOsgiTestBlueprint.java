72b55af61ab1c41bf9de9fcaa6cdba15666b59f7
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2019-11-09 13:59:38 +0100

    messing about with various EIP things and general Camel builder syntax

--- src/main/java/osgi/CamelOsgiTestBlueprint.java
+++ src/main/java/osgi/CamelOsgiTestBlueprint.java
@@ -4,10 +4,39 @@ import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 
 public class CamelOsgiTestBlueprint extends RouteBuilder {
+    public static final String LOGGER = "com.asodc.camel.osgi.CamelOsgiTestBlueprint";
+
+    private static final String VISITED_FIRST_ENDPOINT = "VisitedFirstEndpoint";
+    private static final String VISITED_SECOND_ENDPOINT = "VisitedSecondEndpoint";
+    private static final String VISITED_THIRD_ENDPOINT = "VisitedThirdEndpoint";
+
+    private static final String SIMPLE_HEADERS_MESSAGE = "Headers: ${headers}";
+
     @Override
     public void configure() throws Exception {
-        from("timer:start?period=100")
-                .id("mainRoute")
-                .log(LoggingLevel.INFO, "com.asodc.camel.osgi.CamelOsgiTestBlueprint", "Headers: ${headers}");
+        from("timer:timer?period=5000")
+                .id("timer")
+                .setHeader(VISITED_FIRST_ENDPOINT, constant(false))
+                .setHeader(VISITED_SECOND_ENDPOINT, constant(false))
+                .setHeader(VISITED_THIRD_ENDPOINT, constant(false))
+                .multicast()
+                .parallelProcessing()
+                .to("direct:firstEndpoint", "direct:secondEndpoint", "direct:thirdEndpoint");
+//                .log(LoggingLevel.INFO, LOGGER, SIMPLE_HEADERS_MESSAGE);
+
+        from("direct:firstEndpoint")
+                .id("firstEndpoint")
+                .setHeader(VISITED_FIRST_ENDPOINT, constant(true))
+                .log(LoggingLevel.INFO, LOGGER, SIMPLE_HEADERS_MESSAGE);
+
+        from("direct:secondEndpoint")
+                .id("secondEndpoint")
+                .setHeader(VISITED_SECOND_ENDPOINT, constant(true))
+                .log(LoggingLevel.INFO, LOGGER, SIMPLE_HEADERS_MESSAGE);
+
+        from("direct:thirdEndpoint")
+                .id("thirdEndpoint")
+                .setHeader(VISITED_THIRD_ENDPOINT, constant(true))
+                .log(LoggingLevel.INFO, LOGGER, SIMPLE_HEADERS_MESSAGE);
     }
 }
